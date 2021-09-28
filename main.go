7cf138dbe3644aa03a469cea4434dc458c2a754c
nomadcoderkor <nomadcoder.kor@gmail.com>
2021-09-28 23:37:59 +0900

    Refactoring

--- main.go
+++ main.go
@@ -1,52 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"text/template"
-
-	"github.com/nomadcoderkor/dinocoin/blockchain"
+	"github.com/nomadcoderkor/dinocoin/explorer"
 )
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	// tmpl := template.Must(template.ParseFiles("templates/page/home.gohtml"))
-	// tmpl.Execute(rw, data)
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
-func add(rw http.ResponseWriter, r *http.Request) {
-	// data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	// GET, POST 분기하여 처리
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		// data := r.FormValue("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-
-}
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Starting Server http://localhost%s", port)
-	http.ListenAndServe(port, nil)
+	explorer.Start()
 }
